58ada3941f20632eb84e302fabbfd4e342fe8f0f
Akanbi Omolade <omoladegraham@gmail.com>
2025-01-27 15:44:50 +0100

    Deploying Quiz App

---
+++ script.js
@@ -0,0 +1,20 @@
+const questions = [
+  {
+    question: "What is the capital of France?",
+    answers: [
+      { text: "Rome", correct: false },
+      { text: "Madrid", correct: false },
+      { text: "Paris", correct: true },
+      { text: "Berlin", correct: false },
+    ],
+  },
+  {
+    question: "What does HTML stands for?",
+    answers: [
+      { text: "Hyper Text Markuup Language", correct: true },
+      { text: "High Tech Machine Learning", correct: false },
+      { text: "Hyperlink and Text Managing Language", correct: false },
+      { text: "Hyper Transfer Markup Language", correct: false },
+    ],
+  },
+];
