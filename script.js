7719907371549d64f80aefc56c1d89e798346f19
Akanbi Omolade <omoladegraham@gmail.com>
2025-01-27 16:02:05 +0100

    App Update

--- script.js
+++ script.js
@@ -17,4 +17,26 @@ const questions = [
       { text: "Hyper Transfer Markup Language", correct: false },
     ],
   },
+  {
+    question: "In Java, which keyword is used to define a class?",
+    answers: [
+      { text: "Function", correct: false },
+      { text: "Class", correct: true },
+      { text: "Public", correct: false },
+      { text: "Static", correct: false },
+    ],
+  },
+  {
+    question: "What is the chemical symbol for water?",
+    answers: [
+      { text: "H20", correct: true },
+      { text: "CO2", correct: false },
+      { text: "O2", correct: false },
+      { text: "H2", correct: false },
+    ],
+  },
 ];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
